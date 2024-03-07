767708414f149588520b52e49b5b58c5d6256587
Alciliano da Silva Lima <alcilianodasilva@gmail.com>
2024-03-07 09:07:22 -0300

    Conceitos iniciais em python

---
+++ aprendendo-python.py
@@ -0,0 +1,9 @@
+nome_empresa = input("Informe o nome da Empresa: ")
+contacao = float(input("Digite a cotação da empresa: "))
+
+valor_dobrado = contacao * 2
+caiu_50 = contacao/2
+print(f"O nome da empresa é: {nome_empresa}")
+print(f"Caso a empresa suba 100% a cotação será de {valor_dobrado}")
+print(f"Caso a empresa caia 50%, o valor da cotação fica: {caiu_50}")
+print(type(nome_empresa))
